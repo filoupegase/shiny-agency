b02fd62d896860f9900bbab49a09216bd326c076
filoupegase <loisoncorentin@yahoo.fr>
2021-12-02 14:15:47 +0100

    feat/useContext

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { createGlobalStyle } from 'styled-components';
+import './index.css';
+
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+      <Router>
+        <GlobalStyle/>
+        <App/>
+      </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 reportWebVitals();
--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import colors from '../../src/utils/style/colors';
+import colors from '../../utils/style/colors';
+import { Loader } from '../../utils/style/Atoms';
 
 
 const SurveyContainer = styled.div`
@@ -28,30 +29,53 @@ const LinkWrapper = styled.div`
   & a:first-of-type {
     margin-right: 20px;
   }
-  `;
+`;
 
 function Survey() {
   const { questionNumber } = useParams();
   const questionNumberInt = parseInt(questionNumber);
-  console.log(questionNumber);
   const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
   const nextQuestionNumber = questionNumberInt + 1;
   const [isDataLoading, setDataLoading] = useState(false);
   const [surveyData, setSurveyData] = useState({});
+  const [error, setError] = useState(null);
+
+  /*useEffect(() => {
+    setDataLoading(true);
+    fetch(`http://localhost:8000/survey`)
+        .then((response) => response.json())
+        .then(({ surveyData }) => {
+          setSurveyData(surveyData);
+          setDataLoading(false);
+        })
+        .catch((error) => console.log(error));
+  }, []);*/
 
   useEffect(() => {
-    setDataLoading(true);
-    fetch('http://localhost:8000/survey')
-        .then((response) => response.json()
-            .then(({ surveyData }) => setSurveyData(surveyData))
-            .catch((error) => console.log(error))
-        );
+    async function fetchSurvey() {
+      setDataLoading(true);
+      try {
+        const response = await fetch(`http://localhost:8000/survey`);
+        const { surveyData } = await response.json();
+        setSurveyData(surveyData);
+      } catch {
+        console.log(error);
+        setError(true);
+      } finally {
+        setDataLoading(false);
+      }
+    }
+    fetchSurvey();
   }, []);
+
 
   return (
       <SurveyContainer>
-        <QuestionTitle>Question : { questionNumber }</QuestionTitle>
-        <QuestionContent>{ surveyData[questionNumber] }</QuestionContent>
+        <QuestionTitle>Question { questionNumber }</QuestionTitle>
+        { isDataLoading ? (
+                <Loader/>)
+            : <QuestionContent>{ surveyData[questionNumber] }</QuestionContent>
+        }
         <LinkWrapper>
           <Link to={ `/survey/${ prevQuestionNumber }` }>Précédent</Link>
           { surveyData[questionNumberInt + 1] ? (
