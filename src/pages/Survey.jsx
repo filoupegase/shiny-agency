07c2602fcaa0c327d61d37ea0a00904fd5c983ba
filoupegase <loisoncorentin@yahoo.fr>
2021-12-02 11:03:22 +0100

    save Get survey

--- src/pages/Survey.jsx
+++ src/pages/Survey.jsx
@@ -1,14 +1,22 @@
-import {useParams} from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Survey() {
-  const { questionNumber } = useParams()
+  const { questionNumber } = useParams();
+  useEffect(() => {
+    fetch('http://localhost:8000/survey')
+        .then((response) => response.json()
+            .then(({ surveyData }) => console.log(surveyData))
+            .catch((error) => console.log(error))
+        );
+  }, []);
 
   return (
-    <div>
-      <h1>Questionnaire 🧮</h1>
-      <h2>Question numéro : {questionNumber}</h2>
-    </div>
-  )
+      <div>
+        <h1>Questionnaire 🧮</h1>
+        <h2>Question numéro : { questionNumber }</h2>
+      </div>
+  );
 }
 
 export default Survey;
